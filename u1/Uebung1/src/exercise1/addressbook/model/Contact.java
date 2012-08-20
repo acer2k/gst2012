e269bf28879781a5f1dc0700919226b0c8d8c4de
acer2k <myacer@gmx.de>
2012-08-20 23:07:37 +0200

    u1 added

---
+++ u1/Uebung1/src/exercise1/addressbook/model/Contact.java
@@ -0,0 +1,15 @@
+/*
+ * Example code used in exercises for lecture "Grundlagen des Software-Testens"
+ * Created and given by Ina Schieferdecker and Edzard Höfig
+ * Freie Universität Berlin, SS 2011
+ */
+package exercise1.addressbook.model;
+
+/**
+ * Markup interface for contact data.
+ * Java does not support an Union concept, but we can approximate on type level.
+ * @author Edzard Hoefig
+ */
+public interface Contact {
+	
+}
