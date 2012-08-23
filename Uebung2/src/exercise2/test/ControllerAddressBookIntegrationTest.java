d4300537b40b054b3a6e5108b0758e24d36782e0
Sascha Gennrich <sascha.gennrich@googlemail.com>
2012-08-23 20:05:21 +0200

    ControllerAddressBookIT: adds asserts

--- Uebung2/src/exercise2/test/ControllerAddressBookIntegrationTest.java
+++ Uebung2/src/exercise2/test/ControllerAddressBookIntegrationTest.java
@@ -5,18 +5,17 @@
  */
 package exercise2.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
 import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 import exercise2.addressbook.controller.AddressBookController;
 import exercise2.addressbook.controller.AddressBookControllerImpl;
@@ -24,6 +23,8 @@ import exercise2.addressbook.controller.ParameterException;
 import exercise2.addressbook.model.AddressBookModel;
 import exercise2.addressbook.model.AddressBookModelImpl;
 import exercise2.addressbook.model.Entry;
+import exercise2.addressbook.model.Gender;
+import exercise2.addressbook.model.PhoneNumber;
 import exercise2.addressbook.model.SizeLimitReachedException;
 import exercise2.addressbook.view.AddressBookView;
 
@@ -32,11 +33,13 @@ import exercise2.addressbook.view.AddressBookView;
  * Integration Test für Addressbook und Controller.
  *
  * Bitte Nummer der Gruppe eintragen:
- * 0
+ * 8
  *
  * Bitte Gruppenmitglieder eintragen:
- * @author Edzard Hoefig
- * @author ...
+ * @author Sascha Gennrich
+ * @author Johannes Klick
+ * @author Felix-Johannes Jendrusch
+ * @author Irena Kpogbezan
  */
 public class ControllerAddressBookIntegrationTest {
 
@@ -65,18 +68,34 @@ public class ControllerAddressBookIntegrationTest {
 	@Before
 	public void setUp() throws Exception {
 		// Instantiate and wire components
-		this.model = new AddressBookModelImpl(addressBookFile);
+		this.model = spy(new AddressBookModelImpl(addressBookFile));
 		this.view = mock(AddressBookView.class);
 		this.controller = new AddressBookControllerImpl(model, view);
 	}
 
 	/**
+	 * Während wir den Test geschrieben haben, ist uns aufgefallen,
+	 * dass die Implementierung von Entry geändert wurde und jetzt
+	 * der Nachname als erster Parameter übergeben wird.
 	 * @see AddressBookControllerTest
 	 */
 	@Test
 	public void add() throws ParameterException, SizeLimitReachedException {
-		this.controller.add("John", "Doe", "M", "123456789", null);
+		// Given
+		String[] johnData = {"John", "Doe", "M", "123456789", null};
+		Entry johnEntry = new Entry("Doe", "John",Gender.Male, new PhoneNumber(123456789));
+		int sizeOfmodel = model.getEntries().size();
+		ArgumentCaptor<Entry> entryAdded = ArgumentCaptor.forClass(Entry.class);
+		
+		// When adding John via controller
+		this.controller.add(
+				johnData[0], johnData[1], johnData[2], johnData[3], johnData[4]);
 	
+		// Then model is updated
+		verify(model).addEntry(entryAdded.capture());
+		assertThat(entryAdded.getValue(), is(johnEntry));
+		assertThat(model.getEntries().size(), is(sizeOfmodel + 1));
+		
 	}
 	
 	@Test(expected=ParameterException.class)
