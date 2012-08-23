05b2988c917f36c4a4b911d49ee29edec6c4b968
acer2k <myacer@gmx.de>
2012-08-23 19:23:37 +0200

    added some tests for task 4  but it is not COMPLETED

--- Uebung2/src/exercise2/test/ControllerAddressBookIntegrationTest.java
+++ Uebung2/src/exercise2/test/ControllerAddressBookIntegrationTest.java
@@ -9,14 +9,21 @@ import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import exercise2.addressbook.controller.AddressBookController;
 import exercise2.addressbook.controller.AddressBookControllerImpl;
 import exercise2.addressbook.controller.ParameterException;
 import exercise2.addressbook.model.AddressBookModel;
 import exercise2.addressbook.model.AddressBookModelImpl;
+import exercise2.addressbook.model.Entry;
 import exercise2.addressbook.model.SizeLimitReachedException;
 import exercise2.addressbook.view.AddressBookView;
 
@@ -68,12 +75,81 @@ public class ControllerAddressBookIntegrationTest {
 	 */
 	@Test
 	public void add() throws ParameterException, SizeLimitReachedException {
-		// Like AddressBookControllerTest's basic add test with some additional
-		// asserts on the model?
+		this.controller.add("John", "Doe", "M", "123456789", null);
+	
+	}
+	
+	@Test(expected=ParameterException.class)
+	public void addFirstNameNull() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add(null, "Doe", "M", null, "john@doe.me");
+	}
+
+	@Test(expected=ParameterException.class)
+	public void addLastNameNull() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add("John", null, "M", null, "john@doe.me");
+	}
+
+	// This test fails caused by a NULL-Pointer exception.
+	@Test(expected=ParameterException.class)
+	public void addGenderNull() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add("John", "Doe", null, null, "john@doe.me");
+	}
+
+	@Test(expected=ParameterException.class)
+	public void addGenderInvalid() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add("John", "Doe", "K", null, "john@doe.me");
+	}
+	
+	@Test
+	public void addPhoneLeadingZero() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add("John", "Doe", "M", "0123456789", null);
+
 	}
 
 	@Test
+	public void addPhoneBigNumber() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add("John", "Doe", "M", "123333333333333333333334", null);
+
+	}
+
+	@Test(expected=ParameterException.class)
+	public void addPhoneNegativeNumber() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add("John", "Doe", "M", "-123456789", null);
+	}
+
+	@Test(expected=ParameterException.class)
+	public void addPhoneNonDigits() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add("John", "Doe", "M", "ABC", null);
+	}
+
+	@Test(expected=ParameterException.class)
+	public void addPhoneAndEmail() throws ParameterException,
+			SizeLimitReachedException {
+		this.controller.add("John", "Doe", "M", "123456789", "john@doe.me");
+	}
+
+	/* TODO
+	@Test(expected=SizeLimitReachedException.class)
+	public void addFull() throws ParameterException,
+			SizeLimitReachedException {
+		doThrow(new SizeLimitReachedException("Size limit reached"))
+				.when(this.model).addEntry(any(Entry.class));
+		this.controller.add("John", "Doe", "M", null, "john@doe.me");
+	}
+    */
+	@Test
+	
+	
 	public void remove() {
+		
 		// Entries are ordered by surname and firstname (ascending).
 	}
 
