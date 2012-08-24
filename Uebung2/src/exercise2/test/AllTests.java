d0778bc540db0245b2b2909a4d45027d6add6495
Felix-Johannes Jendrusch <felixjendrusch@googlemail.com>
2012-08-24 13:15:25 +0200

    Add header comment to AllTest

--- Uebung2/src/exercise2/test/AllTests.java
+++ Uebung2/src/exercise2/test/AllTests.java
@@ -4,9 +4,20 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+/**
+ * Uebung 2 - Komponenten und Integrationstest
+ * JUnit-TestSuite für Komponenten- und Integrationstest
+ *
+ * Bitte Nummer der Gruppe eintragen:
+ * 8
+ *
+ * Bitte Gruppenmitglieder eintragen:
+ * @author Sascha Gennrich
+ * @author Johannes Klick
+ * @author Felix-Johannes Jendrusch
+ * @author Irena Kpogbezan
+ */
 @RunWith(Suite.class)
 @SuiteClasses({ AddressBookControllerTest.class,
-	ControllerAddressBookIntegrationTest.class})
-public class AllTests {
-
-}
+    ControllerAddressBookIntegrationTest.class })
+public class AllTests {}
